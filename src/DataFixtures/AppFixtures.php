5b61291e79a23b6372cb87a26dd0422f5d382874
MerciMathieu <mathieu.delclos@gmail.com>
2020-11-24 14:27:22 +0100

    fixtures and migration

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Platform;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $platform = new Platform();
+        for ($i = 0; $i < 5; $i++) {
+            $platform->setName("platform$i")
+                ->setUrl("www.testurl.platform$i.com");
+
+            $manager->persist($platform);
+        }
+
 
         $manager->flush();
     }
