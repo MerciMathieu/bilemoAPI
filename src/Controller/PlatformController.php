fafbee75c9d5f1b4714220843a95963c730c2e04
MerciMathieu <mathieu.delclos@gmail.com>
2020-12-11 17:50:19 +0100

    delete method + validation

--- src/Controller/PlatformController.php
+++ src/Controller/PlatformController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\PlatformRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,5 +25,38 @@ class PlatformController extends AbstractController
         );
 
         return new Response($platformsJson);
+    }
+
+    /**
+     * @Route("/bilemo/platforms/{platformId<\d+>}/users/delete/{userId<\d+>}", name="delete_user", methods={"DELETE"})
+     */
+    public function deleteUser(PlatformRepository $platformRepository, int $platformId, UserRepository $userRepository, int $userId, SerializerInterface $serializer, EntityManagerInterface $manager): Response
+    {
+        $platform = $platformRepository->find($platformId);
+        if ($platform == null) {
+            return new Response(
+                "La plateforme '$platformId' n'existe pas!",
+                404,
+                ["ContentType" => "application/json"]
+            );
+        }
+
+        $user = $userRepository->find($userId);
+        if ($user == null) {
+            return new Response(
+                "L'utilisateur '$userId' n'existe pas!",
+                404,
+                ["ContentType" => "application/json"]
+            );
+        }
+
+        $platform->removeUser($user);
+        $manager->flush();
+
+        return new Response(
+            "L'utilisateur a bien été supprimé!",
+            200,
+            ["ContentType" => "application/json"]
+        );
     }
 }
