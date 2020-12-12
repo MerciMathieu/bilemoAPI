eb069cab21ab87a532f3f0dd681a93c9a38f1dcd
MerciMathieu <mathieu.delclos@gmail.com>
2020-12-12 12:50:28 +0100

    refacto post method

--- src/Controller/PlatformController.php
+++ src/Controller/PlatformController.php
@@ -7,6 +7,7 @@ use App\Repository\PlatformRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -33,37 +34,29 @@ class PlatformController extends AbstractController
     /**
      * @Route("/bilemo/platforms/{platformId<\d+>}/users/create", name="user_create", methods={"POST"})
      */
-    public function addUser(UserRepository $userRepository, SerializerInterface $serializer, int $platformId, PlatformRepository $platformRepository, Request $request, EntityManagerInterface $manager, ValidatorInterface $validator): Response
+    public function addUser(SerializerInterface $serializer, int $platformId, PlatformRepository $platformRepository, Request $request, EntityManagerInterface $manager, ValidatorInterface $validator): Response
     {
-        if (!$request->get('email')) {
-            return new Response("Pour ajouter un utilisateur, vous devez entrer son email en paramètre.\nExemple: /users/create?EMAIL=TEST@EMAIL.FR", 500, [
-                'Content-Type' => 'application/json'
-            ]);
-        }
-        $email = $request->get('email');
-
         $platform = $platformRepository->find($platformId);
+
         if (!$platform || $platform == null ) {
-            $errorMessage = "L'identifiant de la plateforme est inconnu";
-            return new Response($errorMessage,400, [
-                'Content-Type' => 'application/json'
-            ]);
+            $this->createNotFoundException();
         }
 
-        $newUser = new User();
-        $newUser->setEmail($email);
-        $errors = $validator->validate($newUser);
+        /** @var User $user */
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $violations = $validator->validate($user);
 
-        if(count($errors)) {
-            $errors = $serializer->serialize($errors, 'json');
-            return new Response($errors, 500, [
-                'Content-Type' => 'application/json'
-            ]);
+        if ($violations->count()) {
+            $errorMessages = [];
+            foreach ($violations as $error) {
+                $errorMessages[] = $error->getMessage();
+            }
+
+            return new JsonResponse($errorMessages, 400);
         }
 
-        $platform->addUser($newUser);
+        $platform->addUser($user);
         $manager->flush();
-        $user = $userRepository->findOneBy(['email' => $email]);
 
         $userJson = $serializer->serialize(
             $user,
@@ -71,6 +64,6 @@ class PlatformController extends AbstractController
             ['groups' => 'list_users']
         );
 
-        return new Response("L'utilisateur a bien été ajouté.\nVoici ses informations: " . $userJson, 200, ['Content-Type' => 'application/json']);
+        return new Response($userJson, 200, ['Content-Type' => 'application/json']);
     }
 }
