7feded070d0b4a50e70285840a3e7f63e434031e
MerciMathieu <mathieu.delclos@internethic.com>
2021-02-12 17:30:10 +0100

    Pagination

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Pagination\PaginatedCollection;
 use App\Repository\ProductRepository;
 use JMS\Serializer\SerializationContext;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use JMS\Serializer\SerializerInterface;
@@ -19,7 +23,7 @@ use Nelmio\ApiDocBundle\Annotation\Model;
 class ProductController extends ExtendedAbstractController
 {
     /**
-     * @Route("/api/products", name="products", methods={"GET"})
+     * @Route("/api/products/{page<\d+>?1}", name="products", methods={"GET"})
      * @IsGranted("ROLE_USER")
      * @OA\Response(
      *   response=200,
@@ -28,16 +32,49 @@ class ProductController extends ExtendedAbstractController
      * )
      * @Cache(maxage="1 hour", public=true)
      */
-    public function getProducts(ProductRepository $productRepository, SerializerInterface $serializer): Response
+    public function getProducts(ProductRepository $productRepository, SerializerInterface $serializer, Request $request): Response
     {
-        $products = $productRepository->findAll();
+        $page = $request->query->get('page', 1);
+        $route = 'products';
+        $routeParams = array();
+        $createLinkUrl = function($targetPage) use ($route, $routeParams) {
+            return $this->generateUrl($route, array_merge(
+                $routeParams,
+                array('page' => $targetPage)
+            ));
+        };
+
+        $query = $productRepository->findAllQueryBuilder();
+        $adapter = new QueryAdapter($query);
+        $pagerfanta = new Pagerfanta($adapter);
+
+        $pagerfanta->setMaxPerPage(5);
+        $pagerfanta->setCurrentPage($page);
+
+        $products = [];
+        foreach ($pagerfanta->getCurrentPageResults() as $product) {
+            $products[] = $product;
+        }
+
+        $paginatedCollection = new PaginatedCollection(
+            $products,
+            $pagerfanta->getNbResults()
+        );
+
+        $paginatedCollection->addLink('self', $createLinkUrl($page));
+        $paginatedCollection->addLink('first', $createLinkUrl(1));
+        $paginatedCollection->addLink('last', $createLinkUrl($pagerfanta->getNbPages()));
+
+        if ($pagerfanta->hasNextPage()) {
+            $paginatedCollection->addLink('next', $createLinkUrl($pagerfanta->getNextPage()));
+        }
+        if ($pagerfanta->hasPreviousPage()) {
+            $paginatedCollection->addLink('prev', $createLinkUrl($pagerfanta->getPreviousPage()));
+        }
 
         $productsJson = $serializer->serialize(
-            $products,
+            $paginatedCollection,
             'json',
-            SerializationContext::create()->setGroups(
-                ['products_list']
-            )
         );
 
         $response = new Response($productsJson, Response::HTTP_OK, ['Content-Type' => 'application/json']);
